d3da6258b22ea20d3b83c5aa7f5378b96b6186c4
nasibovv <vnasibov7929@ada.edu.az>
2023-04-25 01:16:50 -0400

    admin.py created and manipulated

---
+++ app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from cargosystem.models import User, Currency, Wallet, Country
+
+admin.site.register(User)
+admin.site.register(Currency)
+admin.site.register(Wallet)
+admin.site.register(Country)
+
--- cargosystem/settings.py
+++ cargosystem/settings.py
@@ -72,14 +72,14 @@ WSGI_APPLICATION = 'cargosystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -132,3 +132,5 @@ DATABASES = {'default':
               'HOST': 'db','PORT': 5432, #default port you don't need to mention in docker-compose
               }
                 }
+
+
